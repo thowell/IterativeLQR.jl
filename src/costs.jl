6880a121d16b20fe9ff1d51cbe769db562e1e5d3
simon-lc <simonlc@stanford.edu>
2022-07-25 14:22:04 -0400

    updating Symbolics

--- src/costs.jl
+++ src/costs.jl
@@ -1,10 +1,10 @@
 struct Cost{T}
     #TODO: types for methods
     evaluate
-    gradient_state 
+    gradient_state
     gradient_action
-    hessian_state_state 
-    hessian_action_action 
+    hessian_state_state
+    hessian_action_action
     hessian_action_state
     evaluate_cache::Vector{T}
     gradient_state_cache::Vector{T}
@@ -16,27 +16,30 @@ end
 
 function Cost(f::Function, num_state::Int, num_action::Int; num_parameter::Int=0)
     #TODO: option to load/save methods
-    @variables x[1:num_state], u[1:num_action], w[1:num_parameter]
-    
+    x = Symbolics.variables(:x, 1:num_state)
+    u = Symbolics.variables(:u, 1:num_action)
+    w = Symbolics.variables(:w, 1:num_parameter)
+    # @variables x[1:num_state], u[1:num_action], w[1:num_parameter]
+
     evaluate = num_parameter > 0 ? f(x, u, w) : f(x, u)
     gradient_state = Symbolics.gradient(evaluate, x)
-    gradient_action = Symbolics.gradient(evaluate, u) 
-    hessian_state_state = Symbolics.jacobian(gradient_state, x) 
-    hessian_action_action = Symbolics.jacobian(gradient_action, u) 
-    hessian_action_state = Symbolics.jacobian(gradient_action, x) 
+    gradient_action = Symbolics.gradient(evaluate, u)
+    hessian_state_state = Symbolics.jacobian(gradient_state, x)
+    hessian_action_action = Symbolics.jacobian(gradient_action, u)
+    hessian_action_state = Symbolics.jacobian(gradient_action, x)
 
     evaluate_func = eval(Symbolics.build_function([evaluate], x, u, w)[2])
     gradient_state_func = eval(Symbolics.build_function(gradient_state, x, u, w)[2])
     gradient_action_func = eval(Symbolics.build_function(gradient_action, x, u, w)[2])
     hessian_state_state_func = eval(Symbolics.build_function(hessian_state_state, x, u, w)[2])
     hessian_action_action_func = eval(Symbolics.build_function(hessian_action_action, x, u, w)[2])
-    hessian_action_state_func = eval(Symbolics.build_function(hessian_action_state, x, u, w)[2])  
+    hessian_action_state_func = eval(Symbolics.build_function(hessian_action_state, x, u, w)[2])
 
-    return Cost(evaluate_func, 
-        gradient_state_func, gradient_action_func, 
+    return Cost(evaluate_func,
+        gradient_state_func, gradient_action_func,
         hessian_state_state_func, hessian_action_action_func, hessian_action_state_func,
-        zeros(1), 
-        zeros(num_state), zeros(num_action), 
+        zeros(1),
+        zeros(num_state), zeros(num_action),
         zeros(num_state, num_state), zeros(num_action, num_action), zeros(num_action, num_state))
 end
 
@@ -48,7 +51,7 @@ function cost(costs::Vector{Cost{T}}, states, actions, parameters) where T
         cost.evaluate(cost.evaluate_cache, states[t], actions[t], parameters[t])
         J += cost.evaluate_cache[1]
     end
-    return J 
+    return J
 end
 
 function cost_gradient!(gradient_states, gradient_actions, costs::Vector{Cost{T}}, states, actions, parameters) where T
@@ -65,7 +68,7 @@ function cost_gradient!(gradient_states, gradient_actions, costs::Vector{Cost{T}
 end
 
 function cost_hessian!(hessian_state_state, hessian_action_action, hessian_action_state, costs::Vector{Cost{T}}, states, actions, parameters) where T
-    H = length(costs) 
+    H = length(costs)
     for (t, cost) in enumerate(costs)
         cost.hessian_state_state(cost.hessian_state_state_cache, states[t], actions[t], parameters[t])
         @views hessian_state_state[t] .+= cost.hessian_state_state_cache
--- src/dynamics.jl
+++ src/dynamics.jl
@@ -1,12 +1,12 @@
 struct Dynamics{T}
-    evaluate 
-    jacobian_state 
+    evaluate
+    jacobian_state
     jacobian_action
-    num_next_state::Int 
-    num_state::Int 
+    num_next_state::Int
+    num_state::Int
     num_action::Int
     num_parameter::Int
-    evaluate_cache::Vector{T} 
+    evaluate_cache::Vector{T}
     jacobian_state_cache::Matrix{T}
     jacobian_action_cache::Matrix{T}
 end
@@ -15,7 +15,11 @@ Model{T} = Vector{Dynamics{T}} where T
 
 function Dynamics(f::Function, num_state::Int, num_action::Int; num_parameter::Int=0)
     #TODO: option to load/save methods
-    @variables x[1:num_state], u[1:num_action], w[1:num_parameter] 
+    x = Symbolics.variables(:x, 1:num_state)
+    u = Symbolics.variables(:u, 1:num_action)
+    w = Symbolics.variables(:w, 1:num_parameter)
+    # @variables x[1:num_state], u[1:num_action], w[1:num_parameter]
+
     y = num_parameter > 0 ? f(x, u, w) : f(x, u)
     jacobian_state = Symbolics.jacobian(y, x);
     jacobian_action = Symbolics.jacobian(y, u);
@@ -24,18 +28,18 @@ function Dynamics(f::Function, num_state::Int, num_action::Int; num_parameter::I
     jacobian_action_func = eval(Symbolics.build_function(jacobian_action, x, u, w)[2]);
     num_next_state = length(y)
 
-    return Dynamics(evaluate_func, jacobian_state_func, jacobian_action_func, 
-                    num_next_state, num_state, num_action, num_parameter, 
+    return Dynamics(evaluate_func, jacobian_state_func, jacobian_action_func,
+                    num_next_state, num_state, num_action, num_parameter,
                     zeros(num_next_state), zeros(num_next_state, num_state), zeros(num_next_state, num_action))
 end
 
-function dynamics!(d::Dynamics, state, action, parameter) 
+function dynamics!(d::Dynamics, state, action, parameter)
     d.evaluate(d.evaluate_cache, state, action, parameter)
     return d.evaluate_cache
 end
 
 function jacobian!(jacobian_states, jacobian_actions, dynamics::Vector{Dynamics{T}}, states, actions, parameters) where T
-    for (t, d) in enumerate(dynamics) 
+    for (t, d) in enumerate(dynamics)
         d.jacobian_state(d.jacobian_state_cache, states[t], actions[t], parameters[t])
         d.jacobian_action(d.jacobian_action_cache, states[t], actions[t], parameters[t])
         @views jacobian_states[t] .= d.jacobian_state_cache
@@ -48,9 +52,9 @@ end
 num_trajectory(dynamics::Vector{Dynamics{T}}) where T = sum([d.num_state + d.num_action for d in dynamics]) + dynamics[end].num_next_state
 
 # user-provided dynamics and gradients
-function Dynamics(f::Function, fx::Function, fu::Function, num_next_state::Int, num_state::Int, num_action::Int, 
-    num_parameter::Int=0)  
-    return Dynamics(f, fx, fu, 
-                    num_next_state, num_state, num_action, num_parameter, 
+function Dynamics(f::Function, fx::Function, fu::Function, num_next_state::Int, num_state::Int, num_action::Int,
+    num_parameter::Int=0)
+    return Dynamics(f, fx, fu,
+                    num_next_state, num_state, num_action, num_parameter,
                     zeros(num_next_state), zeros(num_next_state, num_state), zeros(num_next_state, num_action))
 end
